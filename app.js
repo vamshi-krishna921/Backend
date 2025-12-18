56a098095ffd598a58e9c5985938474243bd3a2a
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-12-18 22:29:15 +0530

    Nodejs module system

---
+++ app.js
@@ -0,0 +1,8 @@
+// global.console.log("Hello, ");
+// globalThis.console.log("World!");
+// globalThis.console.log(module);
+
+// Understanding node.js module system
+const math = require('./math');
+console.log(math.add(10,20));
+console.log(math.sub(20,10));
